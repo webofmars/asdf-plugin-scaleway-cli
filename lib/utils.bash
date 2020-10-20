e84db86869771d5fcc4809f53cd7e17f22c3ef05
Frederic Leger <frederic@webofmars.com>
2020-10-20 10:48:35 +0200

    Initial commit
    Signed-off-by: Frederic Leger <frederic@webofmars.com>

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for scaleway-cli.
 GH_REPO="https://github.com/scaleway/scaleway-cli"
 
 fail() {
@@ -29,7 +28,6 @@ list_github_tags() {
 }
 
 list_all_versions() {
-  # TODO: Adapt this. By default we simply list the tag names from GitHub releases.
   # Change this function if scaleway-cli has other means of determining installable versions.
   list_github_tags
 }
@@ -39,8 +37,28 @@ download_release() {
   version="$1"
   filename="$2"
 
-  # TODO: Adapt the release URL convention for scaleway-cli
-  url="$GH_REPO/archive/v${version}.tar.gz"
+  local target_os target_ext target_arch version_slug
+
+  target_os="unknown"
+  target_arch="x86_64"
+  target_ext=""
+  version_slug="2-2-0"
+
+  case "$OSTYPE" in
+    solaris*) target_os="solaris" ;;
+    darwin*)  target_os="darwin" ;;
+    linux*)   target_os="linux" ;;
+    bsd*)     target_os="bsd" ;;
+    msys*)    target_os="windows"; target_ext=".exe" ;;
+    *)        echo "unknown: $OSTYPE" ;;
+  esac
+
+  target_arch="$(uname -m)"
+  version_slug="$(echo ${version} | tr '.' '-' | cut -d v -f 2)"
+
+  # exemple https://github.com/scaleway/scaleway-cli/releases/download/v2.2.0/scw-2-2-0-darwin-x86_64
+  # exemple scw-2-2-0-darwin-x86_64
+  url="$GH_REPO/releases/download/v${version}/scw-${version_slug}-${target_os}-${target_arch}${target_ext}"
 
   echo "* Downloading scaleway-cli release $version..."
   curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
@@ -50,28 +68,31 @@ install_version() {
   local install_type="$1"
   local version="$2"
   local install_path="$3"
+  local tool_cmd
+  tool_cmd="scw"
 
   if [ "$install_type" != "version" ]; then
     fail "asdf-scaleway-cli supports release installs only"
   fi
 
-  # TODO: Adapt this to proper extension and adapt extracting strategy.
-  local release_file="$install_path/scaleway-cli-$version.tar.gz"
+  local release_file="$install_path/scw-$version"
   (
-    mkdir -p "$install_path"
+    mkdir -p "$install_path/bin"
     download_release "$version" "$release_file"
-    tar -xzf "$release_file" -C "$install_path" --strip-components=1 || fail "Could not extract $release_file"
-    chmod a+x "$install_path/bin/scw"
-    rm "$release_file"
+    # tar -xzf "$release_file" -C "$install_path" --strip-components=1 || fail "Could not extract $release_file"
+    # chmod a+x "$install_path/bin/scw"
+    # rm "$release_file"
+    mv "$release_file" "$install_path/bin/$tool_cmd"
+    chmod a+x "$install_path/bin/$tool_cmd"
 
-    # TODO: Asert scaleway-cli executable exists.
-    local tool_cmd
-    tool_cmd="$(echo "scw version" | cut -d' ' -f1)"
     test -x "$install_path/bin/$tool_cmd" || fail "Expected $install_path/bin/$tool_cmd to be executable."
+
+    # TEST
+    "$install_path/bin/$tool_cmd" version || fail "Expected $install_path/bin/$tool_cmd version to work."
 
     echo "scaleway-cli $version installation was successful!"
   ) || (
-    rm -rf "$install_path"
+    #rm -rf "$install_path/bin/$tool_cmd"
     fail "An error ocurred while installing scaleway-cli $version."
   )
 }
